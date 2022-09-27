159da14ebbf9fde5412ccb535d7f589b9709d31a
carrickda <carrickda@gmail.com>
2022-09-27 02:22:55 +0000

    created js file

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+console. log("Hello World!");
+
+var LeaguePlayer = prompt('Wat is your name?');
+alert('Hello,  ' + LeaguePlayer + ' Click OK to proceed');
+
+
+var Champion = prompt('Do you play League?');
+
+if (Champion == 'no'){
+    alert('You should play it is fun. Click OK to proceed.');
+} else {
+    prompt('Which role?');
+} 
+    
+
+alert('I play Support and Jungle');
