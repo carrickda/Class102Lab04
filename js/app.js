c007ba08f1cdea9b856f5b942585ebebc63b089a
carrickda <carrickda@gmail.com>
2022-09-29 06:06:43 +0000

    put in function and moves js script into head

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
+
 'use strict';
 console. log("Hello World!");
 
-var LeaguePlayer = prompt('Wat is your name?');
+var LeaguePlayer = prompt('What is your name?');
 alert('Hello,  ' + LeaguePlayer + ' Click OK to proceed');
 
+function playLeague(msg, target){
+   var role = prompt('Do you play League?');
+   console. log('prompt question', role);
 
-var Champion = prompt('Do you play League?');
+   var answer;
 
-if (Champion == 'no'){
-    alert('You should play it is fun. Click OK to proceed.');
+if (role == 'no'){
+    answer=('You should play it is fun. Click OK to proceed.');
+    alert(answer);
 } else {
     prompt('Which role?');
 } 
-    
+}    
+
+playLeague('Hello', 1);
 
 alert('I play Support and Jungle');
+
